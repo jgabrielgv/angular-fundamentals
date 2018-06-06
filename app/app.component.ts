9a37936dd82b548f19b173b7b35f9653dee0bcb1
jgabrielgv <jgmezvargas@gmail.com>
2018-06-06 17:01:25 -0600

    Rendering flows 2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,92 @@
 import { Component } from '@angular/core';
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <input
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [class.checked-in]="passenger.checkedIn">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [ngClass]="{
+            'checked-in': passenger.checkedIn,
+            'checked-out': !passenger.checkedIn
+          }">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71': '#c0392b')">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span class="status"
+          [ngStyle]="{
+            'backgroundColor': (passenger.checkedIn ? '#2ecc71': '#c0392b')
+          }">
+          </span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+    </div>
+  `
+})
+export class AppComponent {
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }
+  ];
+}
+
+
+/**
+   * name: string = '';
+  handleChange(value: string) {
+    this.name = value;
+  }
+   * <input
         type="text"
         [value]="name"
         (input)="handleChange($event.target.value)">
@@ -20,12 +101,13 @@ import { Component } from '@angular/core';
         Searching for... {{ name }}
       </div>
 
-    </div>
-  `
-})
-export class AppComponent {
-  name: string = '';
-  handleChange(value: string) {
-    this.name = value;
-  }
-}
+      <h3>Airline Passengers</h3>
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </template>
+      </ul>
+   *
+  */
