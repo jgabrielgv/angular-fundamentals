ff7306739e465d1cffe8cc319f841b814ada5aae
jgabrielgv <jgmezvargas@gmail.com>
2018-06-06 15:36:42 -0600

    Rendering flows

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,27 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-     {{ title }}
+      <input
+        type="text"
+        [value]="name"
+        (input)="handleChange($event.target.value)">
+
+      <template [ngIf]="name.length > 2">
+        <div>
+          Searching for... {{ name }}
+        </div>
+      </template>
+
+      <div *ngIf="name.length > 2">
+        Searching for... {{ name }}
+      </div>
+
     </div>
   `
 })
 export class AppComponent {
-  title: string;
-  constructor() {
-    this.title = 'Ultimate Angular';
+  name: string = '';
+  handleChange(value: string) {
+    this.name = value;
   }
 }
